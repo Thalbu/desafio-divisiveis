eca074e3d735bb55bead2e931989106409892697
Thalbu <tainanalbuquerque1@hotmail.com>
2022-10-19 01:15:07 -0300

    feat: usa funcoes

--- src/services/buzz.js
+++ src/services/buzz.js
@@ -1,6 +1,12 @@
 "use strict";
 
+const core = require("../core");
+
 module.exports = (req, res) => {
-  console.log(req.params);
-  res.send(`<h1 style="color: blue">${100}</h1>`);
+  const arrayNumeros = core.geradorArraySequencia(100);
+  const arrayDivisiveisPor5 = core.divisiveisPor5(arrayNumeros);
+
+  res.send(
+    `<h1 style="color: blue">Rota Buzz:</h1><h2>Nessa rota teremos os números de 1 a 100 que são divisíveis por 5</h2><h3>Resposta: ${arrayDivisiveisPor5}</h3>`
+  );
 };
--- src/services/fizz-buzz.js
+++ src/services/fizz-buzz.js
@@ -4,6 +4,9 @@ const core = require("../core");
 
 module.exports = (req, res) => {
   const arrayNumeros = core.geradorArraySequencia(100);
-  console.log(arrayNumeros);
-  res.send(`<h1 style="color: blue">Porta 3000/FizzBuzz ${arrayNumeros}</h1>`);
+  const arrayDivisiveisPor3e5 = core.divisiveisPor3e5(arrayNumeros);
+
+  res.send(
+    `<h1 style="color: blue">Rota FizzBuzz:</h1><h2>Nessa rota teremos os números de 1 a 100 que são divisíveis por 3 e por 5 ao mesmo tempo</h2><h3>Resposta: ${arrayDivisiveisPor3e5}</h3>`
+  );
 };
